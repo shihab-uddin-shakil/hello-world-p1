491bd3dd39ea44e75b8f2b1abeeeb31670eeca1c
kawsurilu <kawsurilu@yahoo.com>
2021-02-14 10:36:34 +0600

    complete php validation

--- RegistrationFormSelf.php
+++ RegistrationFormSelf.php
@@ -7,30 +7,68 @@
 
 	<h1>Registration Form Self</h1>
 
+	<?php
+		$firstNameErr = $lastNameErr = $websiteErr = "";
+		$firstName = ""; 
+		$lastName = ""; 
+		$website = "";
+
+		if($_SERVER["REQUEST_METHOD"] == "POST") {
+			if(empty($_POST['fname'])) {
+				$firstNameErr = "Please fill up the first name properly";
+			}
+			else {
+				$firstName = $_POST['fname'];
+			}
+
+			if(empty($_POST['lname'])) {
+				$lastNameErr = "Please fill up the last name properly";
+			}
+			else {
+				$lastName = $_POST['lname'];
+			}
+
+			if(empty($_POST['website'])) {
+				$websiteErr = "Please fill up the website url properly";
+			}
+			else {
+				$website = $_POST['website'];
+			}
+
+			if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
+  				$websiteErr = "Invalid URL";
+			}
+			else {
+				$website = $_POST['website'];
+			}
+		}
+	?>
+
 	<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
 		
 		<!-- Input Text Field -->
 		<label for="fname">First Name</label>
-		<input type="text" name="fname" id="fname">
+		<input type="text" name="fname" id="fname" value="<?php echo $firstName; ?>"> 
+		<p style="color:red"><?php echo $firstNameErr; ?></p>
 
 		<br>
 
 		<label for="lname">Last Name</label>
-		<input type="text" name="lname" id="lname">
+		<input type="text" name="lname" id="lname" value="<?php echo $lastName ?>">
+		<p><?php echo $lastNameErr; ?></p>
+		
+		<br>
 
+		<label for="website">Website</label>
+		<input type="url" name="website" id="website" value="<?php echo $website ?>">
+		<p><?php echo $websiteErr; ?></p>
+		
 		<br>
 
 		<!-- Input submit -->
 		<input type="submit" value="Submit">
 
 	</form>
-
-	<?php 	
-
-		if($_SERVER["REQUEST_METHOD"] == "POST") {
-			echo $_POST['fname'] . " " . $_POST['lname'];
-		}
-	?>
 
 </body>
 </html>
